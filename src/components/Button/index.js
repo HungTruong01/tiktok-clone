b3a700d83a5359e8cf5e77138a394b372fc1a6b1
HungTruong01 <133994182+HungTruong01@users.noreply.github.com>
2024-02-28 23:46:24 +0700

    update

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,62 @@
 import classNames from "classnames/bind";
 import styles from "./Button.module.scss";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-function Button({to, href, children, onClick}) {
-    let Comp = 'button';
+function Button({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  text = false,
+  disabled = false,
+  rounded = false,
+  small = false,
+  large = false,
+  children,
+  onClick,
+  className,
+  ...passProp
+}) {
+  let Comp = "button";
+  const props = {
+    onClick,
+    ...passProp,
+  };
 
-    const classes = cx('wrapper');
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith("on") && typeof props[key] === "function") {
+        delete props[key];
+      }
+    });
+  }
 
-    return ( 
-        <Comp classNames={classes}>
-            <span>{children}</span>
-        </Comp>
-    );
+  if (to) {
+    props.to = to;
+    Comp = Link;
+  } else if (href) {
+    props.href = href;
+    Comp = "a";
+  }
+
+  const classes = cx("wrapper", {
+    [className]: className,
+    primary,
+    outline,
+    small,
+    large,
+    text,
+    disabled,
+    rounded,
+  });
+
+  return (
+    <Comp className={classes} {...props}>
+      <span>{children}</span>
+    </Comp>
+  );
 }
 
 export default Button;
